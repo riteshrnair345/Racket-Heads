03b05afc35c2708961b1450045f14ef3405ff265
Ritesh R Nair <riteshrnair345@gmail.com>
2026-07-20 13:11:45 +0530

    fix: Allow uploading files with the same name by adding random suffix

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -21,6 +21,7 @@ export async function POST(request: Request): Promise<NextResponse> {
   try {
     const blob = await put(filename, request.body, {
       access: 'public',
+      addRandomSuffix: true,
     });
 
     return NextResponse.json({ success: true, url: blob.url });
